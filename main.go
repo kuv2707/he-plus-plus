e8e04aff1cdad820ab3752fb9be3a596696dc748
Kislay <88523649+kuv2707@users.noreply.github.com>
2023-10-18 10:54:54 +0530

    printing lexemes

--- main.go
+++ main.go
@@ -1,21 +1,21 @@
 package main
 
 import(
-	// "fmt"
+	"fmt"
 	"toylingo/lexer"
-	"toylingo/parser"
+	// "toylingo/parser"
 )
 
 func main(){
 
 	var tokens *lexer.Node =lexer.Lexify("./samples/sample.lg")
 	for node:=tokens;node!=nil;node=node.Next{
-		// fmt.Print()
+		fmt.Print(node.Val)
 	}
 	
-	treeNode:=parser.ParseTree(tokens)
+	// treeNode:=parser.ParseTree(tokens)
 
-	parser.PrintTree(treeNode,"")
+	// parser.PrintTree(treeNode,"")
 
 
 
